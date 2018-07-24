49f3306bfa2875fe35589367287640a81deff114
Orens Xhagolli <orens.xhagolli@hotmail.com>
2018-07-23 21:13:59 -0700

    Add comments on example file. More to come

--- example.py
+++ example.py
@@ -6,8 +6,8 @@ from pyphys import core
 class Example(core.EngineBase):
 
     def setup(self):
-        pygame.display.set_mode((640, 480))
-        self.frame_rate = 60
+        pygame.display.set_mode((640, 480))  # Set the size of the screen
+        self.frame_rate = 60  # Set the desired frame rate (remove this if you'd like max frame rate)
 
     def update(self):
         pass
